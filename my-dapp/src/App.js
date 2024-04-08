9974253d374bc2e0deca8cf0720bb5542c776ef6
azharakhter555 <157714944+azharakhter555@users.noreply.github.com>
2024-04-08 02:51:11 +0100

    changeing asd
    asd

--- my-dapp/src/App.js
+++ my-dapp/src/App.js
@@ -20,7 +20,6 @@ function App() {
         <div className="collapse navbar-collapse" id="navbarCollapse">
             <div className="navbar-nav ms-auto py-4 py-lg-0">
                 <a href="index.html" className="nav-item nav-link active">Home</a>
-                <a href="about.html" className="nav-item nav-link">About</a>
                 <a href="service.html" className="nav-item nav-link">Upcomming Envent</a>
                 <a href="roadmap.html" className="nav-item nav-link">Events</a>
             </div>
@@ -80,56 +79,80 @@ function App() {
         </div>
       </div>
       
-      <div className="container-xxl py-5">
-        <div className="container">
-            <div className="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style={{ maxWidth: '1000px' }}>
-                <h1 className="display-6">Roadmap</h1>
-                <p className="text-primary fs-5 mb-5">We Translate Your Dream Into Reality</p>
+          <div className="container-xxl bg-light py-5 my-5" style={{ backgroundImage: `url(${Image2})`, boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+        <div className="container py-5">
+            <div className="text-center mx-auto wow fadeInUp" data-wow-delay="0.1s" style={{ maxWidth: '500px' }}>
+                <h1 className="display-6" style={{ color: 'white', fontWeight: 'bold' }}>Services</h1>
+                <p className="text-primary fs-5 mb-5" style={{ color: 'white', fontWeight: 'bold' }}>Buy, Sell And Exchange Cryptocurrency</p>
             </div>
-            <div className="owl-carousel roadmap-carousel wow fadeInUp" data-wow-delay="0.1s">
-                <div className="roadmap-item">
-                    <div className="roadmap-point"><span></span></div>
-                    <h5>January 2045</h5>
-                    <span>Diam dolor ipsum sit amet erat ipsum lorem sit</span>
+            <div className="row g-4">
+                <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div className="service-item bg-white p-5">
+                        <img className="img-fluid mb-4" src="img/icon-7.png" alt="" />
+                        <h5 className="mb-3">Currency Wallet</h5>
+                        <p>Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo
+                            justo</p>
+                        <a href="">Read More <i className="fa fa-arrow-right ms-2"></i></a>
+                    </div>
                 </div>
-                <div className="roadmap-item">
-                    <div className="roadmap-point"><span></span></div>
-                    <h5>March 2045</h5>
-                    <span>Diam dolor ipsum sit amet erat ipsum lorem sit</span>
+                <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div className="service-item bg-white p-5">
+                        <img className="img-fluid mb-4" src="img/icon-3.png" alt="" />
+                        <h5 className="mb-3">Currency Transaction</h5>
+                        <p>Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo
+                            justo</p>
+                        <a href="">Read More <i className="fa fa-arrow-right ms-2"></i></a>
+                    </div>
                 </div>
-                <div className="roadmap-item">
-                    <div className="roadmap-point"><span></span></div>
-                    <h5>May 2045</h5>
-                    <span>Diam dolor ipsum sit amet erat ipsum lorem sit</span>
+                <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div className="service-item bg-white p-5">
+                        <img className="img-fluid mb-4" src="img/icon-9.png" alt="" />
+                        <h5 className="mb-3">Bitcoin Investment</h5>
+                        <p>Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo
+                            justo</p>
+                        <a href="">Read More <i className="fa fa-arrow-right ms-2"></i></a>
+                    </div>
                 </div>
-                <div className="roadmap-item">
-                    <div className="roadmap-point"><span></span></div>
-                    <h5>July 2045</h5>
-                    <span>Diam dolor ipsum sit amet erat ipsum lorem sit</span>
+                <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div className="service-item bg-white p-5">
+                        <img className="img-fluid mb-4" src="img/icon-5.png" alt="" />
+                        <h5 className="mb-3">Currency Exchange</h5>
+                        <p>Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo
+                            justo</p>
+                        <a href="">Read More <i className="fa fa-arrow-right ms-2"></i></a>
+                    </div>
                 </div>
-                <div className="roadmap-item">
-                    <div className="roadmap-point"><span></span></div>
-                    <h5>September 2045</h5>
-                    <span>Diam dolor ipsum sit amet erat ipsum lorem sit</span>
+                <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <div className="service-item bg-white p-5">
+                        <img className="img-fluid mb-4" src="img/icon-2.png" alt="" />
+                        <h5 className="mb-3">Bitcoin Escrow</h5>
+                        <p>Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo
+                            justo</p>
+                        <a href="">Read More <i className="fa fa-arrow-right ms-2"></i></a>
+                    </div>
                 </div>
-                <div className="roadmap-item">
-                    <div className="roadmap-point"><span></span></div>
-                    <h5>November 2045</h5>
-                    <span>Diam dolor ipsum sit amet erat ipsum lorem sit</span>
+                <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div className="service-item bg-white p-5">
+                        <img className="img-fluid mb-4" src="img/icon-8.png" alt="" />
+                        <h5 className="mb-3">Token Sale</h5>
+                        <p>Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo
+                            justo</p>
+                        <a href="">Read More <i className="fa fa-arrow-right ms-2"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
-      </div>
-      
+    </div>
+    
       
       {/* footer */}
-      <div className="container-fluid bg-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+      <div className="container-fluid bg-light footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s" style={{ backgroundImage: `url(${Image2})`, boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
         <div className="container py-5">
             <div className="row g-5">
                 <div className="col-md-12" style={{ textAlign:"center" }}>
                     <h1 className="text-primary mb-4"><img className="img-fluid me-2" src="img/icon-1.png" alt=""
-                             style={{ width: '45px',textAlign:"center" }}/>Buys Tickets</h1>
-                    <span>Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed
+                             style={{ width: '45px',textAlign:"center",color: 'white', fontWeight: 'bold' }}/>Buys Tickets</h1>
+                    <span style={{ color: 'white', fontWeight: 'bold' }}>Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed
                         stet lorem sit clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum
                         et lorem et sit.</span>
                 </div>
